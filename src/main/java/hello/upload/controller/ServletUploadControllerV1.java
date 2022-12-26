eeaa0df816ce5c8042b96d3d8d182842f3edb078
nayeon-jo <nyc0516@naver.com>
2022-12-26 17:18:53 +0900

    서블릿 파일 업로드

--- src/main/java/hello/upload/controller/ServletUploadControllerV1.java
+++ src/main/java/hello/upload/controller/ServletUploadControllerV1.java
@@ -23,8 +23,7 @@ public class ServletUploadControllerV1 {
     }
 
     @PostMapping("/upload")
-    public String saveFileV1(HttpServletRequest request) throws
-            ServletException, IOException {
+    public String saveFileV1(HttpServletRequest request) throws ServletException, IOException {
         log.info("request={}", request);
         String itemName = request.getParameter("itemName");
         log.info("itemName={}", itemName);
